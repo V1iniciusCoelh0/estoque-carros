4e8d2cf8ceda7cd80a967ce6110fc5f43608af47
V1niciusCoelh0 <viniciusmurce54@gmail.com>
2026-04-08 11:16:52 -0300

    feat: criar package br.com.vinicius.estoque e classe Carro

---
+++ src/main/java/com/V1niciusCoelh0/Carro.java
@@ -0,0 +1,7 @@
+package com.V1niciusCoelh0;
+
+public class Carro {
+    private String modelo;
+    private String placa;
+    private int ano;
+}
---
+++ src/main/java/com/V1niciusCoelh0/Main.java
@@ -0,0 +1,9 @@
+package com.V1niciusCoelh0;
+
+import java.util.ArrayList;
+
+public class Main{
+      void main(){
+        ArrayList<Carro> carros = new ArrayList<Carro>(3);
+    }
+}
